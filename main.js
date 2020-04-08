781a2606e1eb15120bd69310079c711c354c1cee
rabbihosain <56104056+rabbihosain@users.noreply.github.com>
2020-04-08 17:32:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Select Element function
+const selectEement = function(element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectEement('.menu-toggle');
+let body = selectEement('body');
+
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
+//Scroll reveal
+window.sr=ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin: 'left',
+    duration: 1000,
+    distance:'25rem',
+    delay:300
+});
+sr.reveal('.animate-right',{
+    origin: 'right',
+    duration: 1000,
+    distance:'25rem',
+    delay:300
+});
+sr.reveal('.animate-top',{
+    origin: 'top',
+    duration: 1000,
+    distance:'25rem',
+    delay:600
+});
+sr.reveal('.animate-bottom',{
+    origin: 'bottom',
+    duration: 1000,
+    distance:'25rem',
+    delay:600
+});
